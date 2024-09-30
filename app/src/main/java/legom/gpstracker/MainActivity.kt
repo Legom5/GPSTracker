e0299beaebab30a31e2d43743b958d03b1797ebe
Legom5 <19olegka97@mail.ru>
2024-09-30 18:53:10 +0300

    bNav and fragmentContainer

--- app/src/main/java/legom/gpstracker/MainActivity.kt
+++ app/src/main/java/legom/gpstracker/MainActivity.kt
@@ -1,10 +1,7 @@
 package legom.gpstracker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import legom.gpstracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
