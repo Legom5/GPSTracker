c7f90844feae8ad0f6bfe7ab490a76b36b557153
Legom5 <19olegka97@mail.ru>
2024-10-03 20:22:06 +0300

    add extension fun

---
+++ app/src/main/java/legom/gpstracker/utils/Extension.kt
@@ -0,0 +1,19 @@
+package legom.gpstracker.utils
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import legom.gpstracker.R
+
+fun Fragment.openFragment(f: Fragment) {
+    (activity as AppCompatActivity).supportFragmentManager
+        .beginTransaction()
+        .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+        .replace(R.id.placeHolder, f).commit()
+}
+
+fun AppCompatActivity.openFragment(f: Fragment) {
+    supportFragmentManager
+        .beginTransaction()
+        .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+        .replace(R.id.placeHolder, f).commit()
+}
