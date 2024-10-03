2ad1481df758ccc814b58cf93028e92f21bd8b89
Legom5 <19olegka97@mail.ru>
2024-10-03 20:29:52 +0300

    selected fragment

--- app/src/main/java/legom/gpstracker/utils/Extension.kt
+++ app/src/main/java/legom/gpstracker/utils/Extension.kt
@@ -1,5 +1,6 @@
 package legom.gpstracker.utils
 
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import legom.gpstracker.R
@@ -16,4 +17,11 @@ fun AppCompatActivity.openFragment(f: Fragment) {
         .beginTransaction()
         .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
         .replace(R.id.placeHolder, f).commit()
+}
+
+fun Fragment.showToast(s: String){
+    Toast.makeText(activity, s, Toast.LENGTH_SHORT).show()
+}
+fun AppCompatActivity.showToast(s: String){
+    Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
 }
