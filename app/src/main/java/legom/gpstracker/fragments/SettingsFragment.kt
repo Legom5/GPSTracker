41cff39395fc34e313021a420339698e0e938f5e
Legom5 <19olegka97@mail.ru>
2024-10-04 17:10:42 +0300

    add change listener

--- app/src/main/java/legom/gpstracker/fragments/SettingsFragment.kt
+++ app/src/main/java/legom/gpstracker/fragments/SettingsFragment.kt
@@ -1,11 +1,36 @@
 package legom.gpstracker.fragments
 
 import android.os.Bundle
+import androidx.preference.Preference
+import androidx.preference.Preference.OnPreferenceChangeListener
 import androidx.preference.PreferenceFragmentCompat
 import legom.gpstracker.R
+import legom.gpstracker.utils.showToast
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
+    private lateinit var timePref: Preference
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.main_preference, rootKey)
+        init()
+    }
+
+    private fun init() {
+        timePref = findPreference("update_time_key") ?: throw RuntimeException("Unknown key")
+        val changeListener = onChangeListener()
+        timePref.onPreferenceChangeListener = changeListener
+    }
+
+    private fun onChangeListener(): OnPreferenceChangeListener {
+        return Preference.OnPreferenceChangeListener { pref, newValue ->
+            val nameArray = resources.getStringArray(R.array.loc_time_update_name)
+            val valueArray = resources.getStringArray(R.array.loc_time_update_value)
+
+            val title = pref.title.toString().substringBefore(":")
+            pref.title = "$title: ${nameArray[valueArray.indexOf(newValue)]}"
+
+            true
+        }
     }
 }
