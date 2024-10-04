12d525bf31a7678331cdbebf4a1c834b34312cd1
Legom5 <19olegka97@mail.ru>
2024-10-04 17:21:02 +0300

    add initPrefs()

--- app/src/main/java/legom/gpstracker/fragments/SettingsFragment.kt
+++ app/src/main/java/legom/gpstracker/fragments/SettingsFragment.kt
@@ -20,17 +20,26 @@ class SettingsFragment : PreferenceFragmentCompat() {
         timePref = findPreference("update_time_key") ?: throw RuntimeException("Unknown key")
         val changeListener = onChangeListener()
         timePref.onPreferenceChangeListener = changeListener
+        initPrefs()
     }
 
     private fun onChangeListener(): OnPreferenceChangeListener {
         return Preference.OnPreferenceChangeListener { pref, newValue ->
             val nameArray = resources.getStringArray(R.array.loc_time_update_name)
             val valueArray = resources.getStringArray(R.array.loc_time_update_value)
-
             val title = pref.title.toString().substringBefore(":")
             pref.title = "$title: ${nameArray[valueArray.indexOf(newValue)]}"
-
             true
         }
     }
+
+    private fun initPrefs() {
+        val pref = timePref.preferenceManager.sharedPreferences
+        val nameArray = resources.getStringArray(R.array.loc_time_update_name)
+        val valueArray = resources.getStringArray(R.array.loc_time_update_value)
+        val title = timePref.title
+        timePref.title =
+            "$title: ${nameArray[valueArray.indexOf(pref?.getString("update_time_key", "3000"))]}"
+    }
+
 }
