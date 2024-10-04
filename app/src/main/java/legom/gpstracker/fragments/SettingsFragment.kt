08a9ce9226d093cf821a390d48de6331c3544f6b
Legom5 <19olegka97@mail.ru>
2024-10-04 16:49:09 +0300

    add update time setting

--- app/src/main/java/legom/gpstracker/fragments/SettingsFragment.kt
+++ app/src/main/java/legom/gpstracker/fragments/SettingsFragment.kt
@@ -1,26 +1,11 @@
 package legom.gpstracker.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import legom.gpstracker.databinding.SettingsBinding
+import androidx.preference.PreferenceFragmentCompat
+import legom.gpstracker.R
 
-class SettingsFragment : Fragment() {
-
-    private lateinit var binding: SettingsBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = SettingsBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance() = SettingsFragment()
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.main_preference, rootKey)
     }
 }
