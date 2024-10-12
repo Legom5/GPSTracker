3f63e4b33240ff94201c175951649b0e8d35f644
Legom5 <19olegka97@mail.ru>
2024-10-12 17:03:14 +0300

    update LocationService.kt

--- app/src/main/java/legom/gpstracker/location/LocationService.kt
+++ app/src/main/java/legom/gpstracker/location/LocationService.kt
@@ -1,16 +1,24 @@
 package legom.gpstracker.location
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import legom.gpstracker.MainActivity
+import legom.gpstracker.R
 
-class LocationService: Service() {
+class LocationService : Service() {
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startNotification()
         return START_STICKY
     }
 
@@ -20,5 +28,36 @@ class LocationService: Service() {
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    private fun startNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val nChannel = NotificationChannel(
+                CHANEL_ID,
+                "Location Service",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val nManager = getSystemService(NotificationManager::class.java) as NotificationManager
+            nManager.createNotificationChannel(nChannel)
+        }
+
+        val nIntent = Intent(this, MainActivity::class.java)
+        val pIntent = PendingIntent.getActivity(
+            this,
+            10,
+            nIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val notification = NotificationCompat.Builder(
+            this,
+            CHANEL_ID
+        ).setSmallIcon(R.mipmap.ic_launcher).setContentTitle("Tracker Running!")
+            .setContentIntent(pIntent)
+            .build()
+        startForeground(99, notification)
+    }
+
+    companion object {
+        const val CHANEL_ID = "channel_1"
     }
 }
