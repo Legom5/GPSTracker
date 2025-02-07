cdf5fe1090936c3a69bb92df38c4ea2f6a12b792
Legom5 <19olegka97@mail.ru>
2025-02-07 17:45:17 +0300

    create TrackAdapter and RV

--- app/src/main/java/legom/gpstracker/fragments/TracksFragment.kt
+++ app/src/main/java/legom/gpstracker/fragments/TracksFragment.kt
@@ -5,11 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import legom.gpstracker.MainApp
+import legom.gpstracker.MainViewModel
 import legom.gpstracker.databinding.TracksBinding
+import legom.gpstracker.db.TrackAdapter
 
 class TracksFragment : Fragment() {
 
     private lateinit var binding: TracksBinding
+
+    private lateinit var adapter: TrackAdapter
+
+    private val model: MainViewModel by activityViewModels{
+        MainViewModel.ViewModelFactory((requireContext().applicationContext as MainApp).database)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +29,27 @@ class TracksFragment : Fragment() {
         binding = TracksBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+        getTracks()
+    }
+
+    private fun getTracks(){
+        model.tracks.observe(viewLifecycleOwner){
+            adapter.submitList(it)
+            binding.tvEmpty.visibility = if (it.isEmpty()) View.VISIBLE else View.GONE
+        }
+    }
+
+    private fun initRcView() = with(binding){
+        adapter = TrackAdapter()
+        rcView.layoutManager = LinearLayoutManager(requireContext())
+        rcView.adapter = adapter
+    }
+
+
 
     companion object {
         @JvmStatic
