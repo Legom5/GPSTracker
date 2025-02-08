56a15552e265e2d552a68a45a64a34d85b63a514
Legom5 <19olegka97@mail.ru>
2025-02-08 16:50:29 +0300

    getPolyLine

--- app/src/main/java/legom/gpstracker/fragments/ViewTrackFragment.kt
+++ app/src/main/java/legom/gpstracker/fragments/ViewTrackFragment.kt
@@ -2,19 +2,20 @@ package legom.gpstracker.fragments
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import legom.gpstracker.MainApp
 import legom.gpstracker.MainViewModel
 import legom.gpstracker.R
-import legom.gpstracker.databinding.FragmentMainBinding
 import legom.gpstracker.databinding.ViewTrackBinding
 import org.osmdroid.config.Configuration
 import org.osmdroid.library.BuildConfig
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.Polyline
 
 class ViewTrackFragment : Fragment() {
 
@@ -45,7 +46,26 @@ class ViewTrackFragment : Fragment() {
             tvTime.text = it.time
             tvAverageVel.text = it.velocity
             tvDistance.text = distance
+            val polyLine = getPolyLine(it.geoPoints)
+            map.overlays.add(polyLine)
+            goToStartPosition(polyLine.actualPoints[0])
         }
+    }
+
+    private fun goToStartPosition(startPosition: GeoPoint){
+        binding.map.controller.zoomTo(15.0)
+        binding.map.controller.animateTo(startPosition)
+    }
+
+    private fun getPolyLine(geoPoints: String): Polyline{
+        val polyLine = Polyline()
+        val list = geoPoints.split("/")
+        list.forEach {
+            if (it.isEmpty()) return@forEach
+            val points = it.split(",")
+            polyLine.addPoint(GeoPoint(points[0].toDouble(), points[1].toDouble()))
+        }
+        return polyLine
     }
 
     private fun settingsOsm() {
