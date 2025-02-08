2f78145a3462714f0d72e5a778331499135867dc
Legom5 <19olegka97@mail.ru>
2025-02-08 17:53:00 +0300

    create Markers

--- app/src/main/java/legom/gpstracker/fragments/ViewTrackFragment.kt
+++ app/src/main/java/legom/gpstracker/fragments/ViewTrackFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import legom.gpstracker.MainApp
@@ -15,6 +16,7 @@ import legom.gpstracker.databinding.ViewTrackBinding
 import org.osmdroid.config.Configuration
 import org.osmdroid.library.BuildConfig
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.Polyline
 
 class ViewTrackFragment : Fragment() {
@@ -48,6 +50,7 @@ class ViewTrackFragment : Fragment() {
             tvDistance.text = distance
             val polyLine = getPolyLine(it.geoPoints)
             map.overlays.add(polyLine)
+            setMarkers(polyLine.actualPoints)
             goToStartPosition(polyLine.actualPoints[0])
         }
     }
@@ -55,6 +58,19 @@ class ViewTrackFragment : Fragment() {
     private fun goToStartPosition(startPosition: GeoPoint){
         binding.map.controller.zoomTo(15.0)
         binding.map.controller.animateTo(startPosition)
+    }
+
+    private fun setMarkers(list: List<GeoPoint>) = with(binding){
+        val startMarker = Marker(map)
+        val finishMarker = Marker(map)
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        finishMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        startMarker.icon = getDrawable(requireContext(), R.drawable.ic_start)
+        finishMarker.icon = getDrawable(requireContext(), R.drawable.ic_finish)
+        startMarker.position = list[0]
+        finishMarker.position = list[list.size - 1]
+        map.overlays.add(startMarker)
+        map.overlays.add(finishMarker)
     }
 
     private fun getPolyLine(geoPoints: String): Polyline{
